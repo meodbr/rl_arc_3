11ed596b27781ab2b68630b8cbdc6a4dceea0eba
Meo Desbois-Renaudin <meo.desbois-renaudin@thalesgroup.com>
2026-02-10 11:02:00 +0100

    training args interface

--- src/rl_arc_3/trainer/interface.py
+++ src/rl_arc_3/trainer/interface.py
@@ -1,5 +1,17 @@
-class TrainingArguments:
-    pass
+from dataclasses import dataclass
+
+
+@dataclass
+class TrainingArgs:
+    num_episodes: int
+    num_workers: int
+    max_steps_per_episode: int
+    log_steps: int
+    save_steps: int
+    max_steps: int
+    lr: float = 1e-3
+    batch_size: int = 128
+    device: str | None = None
 
 
 class TrainerInterface:
