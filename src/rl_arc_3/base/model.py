2d81c9891b47a350604b8552072d8935a1a2425b
Meo Desbois-Renaudin <meo.desbois-renaudin@thalesgroup.com>
2026-02-16 19:06:23 +0100

    state_dict handling for BaseModel

--- src/rl_arc_3/base/model.py
+++ src/rl_arc_3/base/model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+import torch
 import torch.nn as nn
 
 from rl_arc_3.base.clone import Checkpointable
@@ -15,8 +16,16 @@ class BaseModel(nn.Module, Checkpointable):
         raise NotImplementedError
     
     def state_dict(self) -> dict:
-        return Checkpointable.state_dict(self)
+        state = super().state_dict()
+        for k, v in state.items():
+            if torch.is_tensor(v):
+                v = v.clone().detach()
+        for k in ["_init_args", "_init_kwargs"]:
+            state[k] = getattr(self, k, "bad")
+        return state
     
-    def load_state_dict(self, state: dict):
-        return Checkpointable.load_state_dict(self, state)
-        
+    def load_state_dict(self, state_dict, strict = True, assign = False):
+        state = state_dict.copy()
+        del state["_init_args"]
+        del state["_init_kwargs"]
+        return super().load_state_dict(state, strict, assign)
